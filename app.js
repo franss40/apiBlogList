a55ff973945c84fda944507dfe663e367a7c9838
franss <franss40@gmail.com>
2024-03-17 20:25:35 +0100

    4.2 add router

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const Blog = require("./models/blog")
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    return response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const router = require('./controllers/blogs')
+app.use('/api/blogs', router)
 
 module.exports = app
