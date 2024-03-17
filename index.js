ca4eeac37187134043445638c93c1783994d7114
franss <franss40@gmail.com>
2024-03-17 10:46:33 +0100

    4.2. add models

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 const express = require("express")
 const app = express()
-require('dotenv').config()
 const cors = require("cors")
-
-// eslint-disable-next-line no-undef
-const mongoUrl = process.env.MONGODB_URI
-
-const mongoose = require("mongoose")
-
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  url: { type: String, required: true },
-  likes: { type: Number, required: true },
-})
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-const Blog = mongoose.model("Blog", blogSchema)
-
-mongoose.set("strictQuery", false)
-mongoose
-  .connect(mongoUrl)
-  .then(() => console.log("Conexión exitosa a la base de datos"))
-  .catch((error) =>
-    console.error("Error al conectar a la base de datos:", error)
-  )
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
---
+++ models/blog.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+// eslint-disable-next-line no-undef
+const mongoUrl = process.env.MONGODB_URI
+
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  url: { type: String, required: true },
+  likes: { type: Number, required: true },
+})
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+const Blog = mongoose.model("Blog", blogSchema)
+
+mongoose.set("strictQuery", false)
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("Conexión exitosa a la base de datos"))
+  .catch((error) =>
+    console.error("Error al conectar a la base de datos:", error)
+  )
+
+module.exports = Blog
