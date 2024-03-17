094b26d3bdb91f335cc48a8e5b3e6a446373386c
franss <franss40@gmail.com>
2024-03-17 11:20:48 +0100

    4.2 add app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const Blog = require("./models/blog")
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    return response.json(blogs)
+  })
+})
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,27 +1,8 @@
+const app = require('./app')
 const { PORT } = require("./utils/config")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const Blog = require("./models/blog")
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    return response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const logger = require(".//utils/loggers")
 
 const PORT_LISTEN = PORT || 3003
 app.listen(PORT_LISTEN, () => {
-  console.log(`Server running on port ${PORT_LISTEN}`)
+  logger.info(`Server running on port ${PORT_LISTEN}`)
 })
