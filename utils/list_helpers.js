8ef530a215d20d8faa53d8151a0efe0105ea0957
franss <franss40@gmail.com>
2024-03-20 12:18:25 +0100

    exercises 4.4, part 2

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -2,4 +2,9 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const likes = blogs.map(item => item.likes)
+  return likes.reduce((a, b) => a + b, 0)
+}
+
+module.exports = { dummy, totalLikes }
