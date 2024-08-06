0ebd207ddb0d5dea87c81eb7e01269c79db836f8
franss <franss40@gmail.com>
2024-08-06 19:40:34 +0200

    Production in Render

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const middleware = require("./utils/middleware")
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const { PORT } = require("./utils/config")
 const logger = require(".//utils/loggers")
 
-const PORT_LISTEN = PORT || 3003
+const PORT_LISTEN = PORT || 3001
 app.listen(PORT_LISTEN, () => {
   logger.info(`Server running on port ${PORT_LISTEN}`)
 })
